cda50ba4bb9d8ba42d9ce58144b4099ab9f520c2
niols <niolsson@gmail.com>
2023-02-17 13:29:02 +0100

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+
+int main()
+{
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(600, 600, "Car Simulation", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	
+
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
